60d67e8c6286f0701660f7cc98fdf129fd62264e
dragonerd <dragonerdev@gmail.com>
2024-12-25 00:20:54 -0400

    v1.0

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-  webbrowser.open_new('http://127.0.0.1:5000')
+  webbrowser.open_new_tab('http://127.0.0.1:5000')
   app.run(debug=True)
